96b38d11b264bd90961a34b53fed98175d09cba3
tepextepex <tepextepex@gmail.com>
2020-06-16 17:36:29 +0300

    refactored model test

--- main.py
+++ main.py
@@ -4,37 +4,21 @@ import matplotlib.pyplot as plt
 arcticdem_path = "/home/tepex/AARI/Glaciology_2019/lighting/source/dem.tif"
 glacier_outlines_path = "/home/tepex/AARI/Glaciers/Aldegonda albedo/shp/aldegonda_outlines_2018/aldegonda_outlines_2018.shp"
 albedo_path = "/home/tepex/AARI/Glaciers/Aldegonda albedo/gridded/L8_albedo_20190727_crop.tif"  # constant albedo for the whole year
-insolation_path = "/home/tepex/AARI/Glaciers/Aldegonda albedo/gridded/total_insolation_July.tif"
+potential_insolation_path = "/home/tepex/AARI/Glaciers/Aldegonda albedo/gridded/total_insolation_July.tif"
 
-e = Energy(arcticdem_path, glacier_outlines_path, albedo_path, insolation_path)
-# e.show_me(e.base_dem_array)
-# e.show_me(e.constant_albedo)
-# e.show_me(e.constant_insolation)
 """
 Let's test the model
 """
-# t_air, wind_speed, rel_humidity, air_pressure, cloudiness, incoming_shortwave, albedo, z
-t_air = 3  # degree Celsius
+t_air = 3  # degrees Celsius
 wind_speed = 2.5  # meters per second
 rel_humidity = 0.85  # 0-1
-air_pressure = 1013  # hPa
+air_pressure = 1000  # hPa
 cloudiness = 0.7  # 0-1
-incoming_shortwave = 300  # W per sq meter (J per second per sq meter)
-albedo = 0.30  # 0-1
+incoming_shortwave = 250  # W per sq meter (J per second per sq meter)
 z = 1.6  # AWS sensors height above ice surface, meters
 aws_coords = (14, 78, 190)  # coordinates of weather station
+days = 30
 #################
-influx = e.calc_heat_influx(t_air, wind_speed, rel_humidity, air_pressure, cloudiness, incoming_shortwave, albedo, z, aws_coords[2])
-melt = e.calc_ice_melt(influx, 916.7, 30)
-plt.imshow(influx)
-plt.colorbar()
-plt.title("Heat influx, W per sq m")
-plt.savefig("/home/tepex/PycharmProjects/energy/png/influx.png")
-# plt.show()
-
-
-plt.imshow(melt)
-plt.colorbar()
-plt.title("Ice melt, m w.e.")
-plt.savefig("/home/tepex/PycharmProjects/energy/png/ice_melt.png")
-# plt.show()
+e = Energy(arcticdem_path, glacier_outlines_path, albedo_path, potential_insolation_path)
+e.set_aws_data(t_air, wind_speed, rel_humidity, air_pressure, cloudiness, incoming_shortwave, z, aws_coords[2])
+e.run(days)
