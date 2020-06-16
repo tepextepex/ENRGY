2a3478b6b4a5c95f42bae5229cda67ba1f803dec
tepextepex <tepextepex@gmail.com>
2020-06-16 17:37:04 +0300

    refactored model methods and incorporated turbulent heat fluxes

--- model.py
+++ model.py
@@ -1,10 +1,13 @@
 import gdal
 import numpy as np
 import matplotlib.pyplot as plt
+from turbo import calc_turbulent_fluxes
 
 
 class Energy:
-	def __init__(self, base_dem_path, glacier_outlines_path, albedo_path, insolation_path):
+	def __init__(self, base_dem_path, glacier_outlines_path, albedo_path, potential_incoming_radiation_path):
+
+		self.__init_constants()
 
 		self.outlines_path = glacier_outlines_path
 
@@ -12,33 +15,69 @@ class Energy:
 		self.base_dem_array = self._load_raster(base_dem_path, self.outlines_path)
 
 		print("Loading albedo map...")
-		self.constant_albedo = self._load_raster(albedo_path, self.outlines_path, remove_outliers=True)
-		debug_imshow(self.constant_albedo, "Albedo grid (not used)")
+		self.albedo = self._load_raster(albedo_path, self.outlines_path, remove_outliers=True)
+		debug_imshow(self.albedo, "Albedo grid")
 
 		print("Loading insolation map...")
-		self.constant_insolation = self._load_raster(insolation_path, self.outlines_path)
-		debug_imshow(self.constant_insolation, "Total potential incoming radiation (not used)")
+		self.potential_incoming_radiation = self._load_raster(potential_incoming_radiation_path, self.outlines_path)
+		debug_imshow(self.potential_incoming_radiation, "Total potential incoming radiation (not used)")
 
-	def run(self, t_air_aws, wind_speed, rel_humidity, air_pressure, cloudiness, incoming_shortwave, albedo, z, elev_aws, ice_density, days):
+	def __init_constants(self):
+		self.CONST = {
+			"ice_density": 916.7,  # kg m-3
+			"latent_heat_of_fusion": 3.34 * 10 ** 5,  # J kg-1
+			"g": 9.81
+		}
+
+	def set_aws_data(self, t_air_aws, wind_speed, rel_humidity, air_pressure, cloudiness, incoming_shortwave, z, elev_aws):
+		self.t_air_aws = t_air_aws
+		self.wind_speed = wind_speed
+		self.rel_humidity = rel_humidity
+		self.air_pressure = air_pressure
+		self.cloudiness = cloudiness
+		self.incoming_shortwave = incoming_shortwave
+		self.z = z
+		self.elev_aws = elev_aws
+
+		self.Tz = self.t_air_aws + 273.15
+		self.P = self.air_pressure * 100  # Pascals from hPa
+
+	def run(self, days):
 		# heat available for melt:
-		Qm = self.calc_heat_influx(t_air_aws, wind_speed, rel_humidity, air_pressure, cloudiness, incoming_shortwave, albedo, z, elev_aws)
+		Qm = self.calc_heat_influx(self.t_air_aws, self.wind_speed, self.rel_humidity, self.air_pressure, self.cloudiness, self.incoming_shortwave, self.z, self.elev_aws)
 		debug_imshow(Qm, title="Heat available for melt")
+
 		# amount of ice which was melt:
+		ice_density = self.CONST["ice_density"]
 		ice_melt = self.calc_ice_melt(Qm, ice_density, days)
 		debug_imshow(ice_melt, title="Ice melt, m w.e.")
 		return ice_melt
 
-	def calc_heat_influx(self, t_air_aws, wind_speed, rel_humidity, air_pressure, cloudiness, incoming_shortwave, albedo, z, elev_aws):
+	def calc_heat_influx(self, t_air_aws, wind_speed, rel_humidity, air_pressure, cloudiness, incoming_shortwave, z, elev_aws):
 		t_surf = 0  # we assume that surface of melting ice is 0 degree Celsius
 		t_air = self.interpolate_air_t(t_air_aws, elev_aws)
+		debug_imshow(t_air, title="Air temperature, degree Celsius")
 
 		rl = self.calc_longwave(t_air, t_surf, cloudiness)
 		debug_imshow(rl, title="Longwave")
 
-		return rl + incoming_shortwave * (1 - albedo)
+		sensible_flux, latent_flux = calc_turbulent_fluxes(self.z, self.wind_speed, self.Tz, self.P, self.rel_humidity)
+		print("Sensible heat flux is %.1f W m-2" % sensible_flux)
+		print("Latent heat flux is %.1f W m-2" % latent_flux)
 
-	@staticmethod
-	def calc_ice_melt(ice_heat_influx, ice_density, days):
+		sensible_flux_array = self.fill_array_with_one_value(sensible_flux)
+		latent_flux_array = self.fill_array_with_one_value(latent_flux)
+		debug_imshow(sensible_flux_array, title="Sensible heat flux")
+		debug_imshow(latent_flux_array, title="Latent heat flux")
+
+		rs = self.calc_shortwave(incoming_shortwave)
+
+		return rl + rs + sensible_flux_array + latent_flux_array
+
+	def calc_shortwave(self, incoming_shortwave):
+		return incoming_shortwave * (1 - self.albedo)
+
+	def calc_ice_melt(self, ice_heat_influx, ice_density, days):
 		"""
 
 		:param ice_heat_influx:
@@ -46,7 +85,8 @@ class Energy:
 		:param days: number of days
 		:return: thickness of melt ice layer in meters w.e.
 		"""
-		return (ice_heat_influx * 86400) / (ice_density * 3.33 * 10 ** 5) * days
+		latent_heat_of_fusion = self.CONST["latent_heat_of_fusion"]
+		return (ice_heat_influx * 86400) / (ice_density * latent_heat_of_fusion) * days
 
 	@staticmethod
 	def calc_longwave(t_air, t_surf, cloudiness):
@@ -55,13 +95,9 @@ class Energy:
 		return lwd - lwu
 
 	@staticmethod
-	def calc_shortwave(k, potential_incoming_solar_radiation):
-		return potential_incoming_solar_radiation * k
-
-	@staticmethod
 	def _load_raster(raster_path, crop_path, remove_negatives=False, remove_outliers=False):
 		ds = gdal.Open(raster_path)
-		crop_ds = gdal.Warp("", ds, dstSRS="+proj=utm +zone=33 +datum=WGS84 +units=m +no_defs", format="VRT", cutlineDSName=crop_path, outputType=gdal.GDT_Float32, xRes=10, yRes=10)
+		crop_ds = gdal.Warp("", ds, dstSRS="+proj=utm +zone=33 +datum=WGS84 +units=m +no_defs", format="VRT", cutlineDSName=crop_path, cropToCutline=True, outputType=gdal.GDT_Float32, xRes=10, yRes=10)
 		band = crop_ds.GetRasterBand(1)
 		nodata = band.GetNoDataValue()
 		array = band.ReadAsArray()
@@ -70,6 +106,7 @@ class Energy:
 			array[array < 0] = np.nan  # makes sense for albedo which couldn't be negative
 		if remove_outliers:
 			array[array > 1] = np.nan
+		print(array.shape)
 		return array
 
 	@staticmethod
@@ -78,6 +115,24 @@ class Energy:
 		plt.colorbar()
 		plt.show()
 		plt.clf()
+
+	@staticmethod
+	def kWh_to_J(insol):
+		"""
+		Converts amount of energy in [kW*h / day] into [J/day]
+		:param insol:
+		:return:
+		"""
+		return insol * 3.6 * 10 ** 6
+
+	@staticmethod
+	def J_to_W(insol):
+		"""
+		Converts amount of energy in [J/day] into the flux in [W/s]
+		:param insol:
+		:return:
+		"""
+		return insol / 86400
 
 	def interpolate_air_t(self, t_air, elev, v_gradient=None):
 		if v_gradient is None:
@@ -98,6 +153,12 @@ class Energy:
 		:return: array
 		"""
 		return (self.base_dem_array - elev) * v_gradient + value
+
+	def fill_array_with_one_value(self, value):
+		array = np.zeros_like(self.base_dem_array, dtype=np.float32)
+		array[~np.isnan(self.base_dem_array)] = value
+		array[np.isnan(self.base_dem_array)] = np.nan
+		return array
 
 
 def to_kelvin(t_celsius):
