df35a2b01d693c8686686c96c7dca2fcd5c77ec0
tepextepex <tepextepex@gmail.com>
2020-06-23 15:44:20 +0300

    some changes in tests

--- main.py
+++ main.py
@@ -6,19 +6,26 @@ glacier_outlines_path = "/home/tepex/AARI/Glaciers/Aldegonda albedo/shp/aldegond
 albedo_path = "/home/tepex/AARI/Glaciers/Aldegonda albedo/gridded/L8_albedo_20190727_crop.tif"  # constant albedo for the whole year
 potential_insolation_path = "/home/tepex/AARI/Glaciers/Aldegonda albedo/gridded/total_insolation_July.tif"
 
+aws_file = "/home/tepex/PycharmProjects/energy/aws/test_aws_data.csv"
+albedo_maps = {
+	"20190727": "/home/tepex/AARI/Glaciers/Aldegonda albedo/gridded/L8_albedo_20190727_crop.tif",
+	"20190803": "/home/tepex/AARI/Glaciers/Aldegonda albedo/gridded/L8_albedo_20190803_crop.tif"
+}
+
 """
 Let's test the model
 """
-t_air = 3  # degrees Celsius
-wind_speed = 2.5  # meters per second
-rel_humidity = 0.85  # 0-1
-air_pressure = 1000  # hPa
+t_air = 2.46  # degrees Celsius
+wind_speed = 1.81  # meters per second
+rel_humidity = 0.858  # 0-1
+air_pressure = 990.14  # hPa
 cloudiness = 0.8  # 0-1
 incoming_shortwave = 250  # W per sq meter (J per second per sq meter)
 z = 1.6  # AWS sensors height above ice surface, meters
-aws_coords = (14, 78, 190)  # coordinates of weather station
-days = 30
+elev_aws = 290
+xy_aws = (478342, 8655635)  # EPSG:32633
 #################
 e = Energy(arcticdem_path, glacier_outlines_path, albedo_path, potential_insolation_path)
-e.set_aws_data(t_air, wind_speed, rel_humidity, air_pressure, cloudiness, incoming_shortwave, z, aws_coords[2])
-e.run(days)
+# e.set_aws_data(t_air, wind_speed, rel_humidity, air_pressure, cloudiness, incoming_shortwave, z, aws_coords[2])
+# e.run()
+e.model(aws_file=aws_file, albedo_maps=albedo_maps, z=1.6, elev_aws=elev_aws, xy_aws=xy_aws)
