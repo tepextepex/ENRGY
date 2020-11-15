2eb3cd8b4b063f37f2bf45127ffe7a5252cd28ca
tepextepex <tepextepex@gmail.com>
2020-11-15 21:25:40 +0300

    Refactored point and distributed parameters, now they are dataclasses

---
+++ parameter_classes.py
@@ -0,0 +1,130 @@
+import numpy as np
+from dataclasses import dataclass, field
+from turbo import _calc_e_max
+from raster_utils import show_me
+
+
+@dataclass
+class AwsParams:
+    # meteorological parameters:
+    t_air: float = field(metadata={"units": "degree Celsius"})
+    wind_speed: float = field(metadata={"units": "m per s"})
+    pressure: float = field(metadata={"units": "hPa"})
+    rel_humidity: float = field(metadata={"units": "0-1"})
+    cloudiness: float = field(metadata={"units": "0-1"})
+    incoming_shortwave: float = field(metadata={"units": "W per sq m"})
+    # AWS location info:
+    elev: float
+    x: float
+    y: float
+    z: float = 2.0
+    # post-calculated fields:
+    Tz: float = field(init=False, metadata={"units": "Kelvin"})
+    P: float = field(init=False, metadata={"units": "Pa"})
+    e: float = field(init=False, metadata={"units": "Pa"})  # partial water vapour pressure at the AWS
+
+    def __post_init__(self):
+        self.t_surf = 0
+        self.Tz = self.t_air + 273.15
+        self.P = self.pressure * 100  # Pascals from hPa
+        self.e = self.rel_humidity * _calc_e_max(self.Tz, self.P)
+
+    def get_meta(self, field_name):
+        return self.__dataclass_fields__[field_name].metadata
+
+    def get_units(self, field_name):
+        return self.get_meta(field_name)["units"]
+
+
+@dataclass
+class DistributedParams:
+    aws: AwsParams
+    dem: np.array = field(metadata={"units": "m", "desc": "Elevation"})
+    delta_dem: np.array = field(init=False, metadata={"units": "m", "desc": "Elevation difference relative to AWS location"})  # elevation differences relative to aws location
+    t_air: np.array = field(init=False, metadata={"units": "degree Celsius", "desc": "Air temperature"})
+    wind_speed: np.array = field(init=False, metadata={"units": "m per s", "desc": "Wind speed"})
+    pressure: np.array = field(init=False, metadata={"units": "hPa", "desc": "Air pressure"})
+    e: np.array = field(init=False, metadata={"units": "Pa", "desc": "Partial pressure of water vapour"})
+    e_max: np.array = field(init=False, metadata={"units": "Pa", "desc": "Max partial pressure of water vapour"})
+    rel_humidity: np.array = field(init=False, metadata={"units": "0-1", "desc": "Relative air humidity"})
+
+    def __post_init__(self):
+        self.delta_dem = self.dem - self.aws.elev
+        self.Tz = self.t_air + 273.15
+        self.t_air = self.__interpolate_t_air()
+        self.param_to_png("t_air")
+        self.wind_speed = self.__fill_array_with_one_value()
+        self.param_to_png("wind_speed")
+        self.pressure = self.__interpolate_pressure()
+        self.param_to_png("pressure")
+        self.P = self.pressure * 100  # Pascals from hPa
+        self.e = self.__interpolate_e()
+        self.param_to_png("e")
+        self.e_max = _calc_e_max(self.Tz, self.P)
+        self.param_to_png("e_max")
+        self.rel_humidity = np.divide(self.e, self.e_max)  # self.e / self.e_max
+        self.param_to_png("rel_humidity")
+
+    def get_meta(self, field_name):
+        return self.__dataclass_fields__[field_name].metadata
+
+    def get_units(self, field_name):
+        return self.get_meta(field_name)["units"]
+
+    def get_desc(self, field_name):
+        return self.get_meta(field_name)["desc"]
+
+    def __interpolate_t_air(self, v_gradient=None):
+        if v_gradient is None:
+            v_gradient = -0.006  # 6 degrees Celsius or Kelvins per 1 m
+        return self.__interpolate_on_dem(self.aws.t_air, v_gradient)
+
+    def __interpolate_pressure(self, v_gradient=None):
+        if v_gradient is None:
+            v_gradient = -0.1145  # Pa per 1 m
+        return self.__interpolate_on_dem(self.aws.pressure, v_gradient)
+
+    def __interpolate_e(self):
+        """
+        Interpolates partial pressure of water vapour (e)
+        :return: np.array with distributed parameter
+        """
+        # delta_elev = self.dem - self.aws.elev
+        return self.aws.e * 10 ** (-self.delta_dem / 6300)
+
+    def __fill_array_with_one_value(self, value):
+        """
+        Assigns a given value to every non-nan cell of a grid
+        :param value: any float
+        :return: np.array
+        """
+        array = np.zeros_like(self.dem, dtype=np.float32)
+        array[~np.isnan(self.dem)] = value
+        array[np.isnan(self.dem)] = np.nan
+        return array
+
+    def __interpolate_on_dem(self, value, v_gradient):
+        """
+        Interpolates a single measurement of some meteorological parameter
+        based on its vertical gradient and DEM
+        :param value: measured value at the AWS
+        :param v_gradient: vertical gradient
+        :return: np.array with distributed parameter
+        """
+        return value + self.delta_dem * v_gradient
+
+    def param_to_png(self, param_name):
+        array = getattr(self, param_name)
+        title = self.get_desc(param_name)
+        units = self.get_units(param_name)
+        show_me(array, title=title, units=units, show=False, verbose=True)
+
+
+if __name__ == "__main__":
+    test_aws_params = AwsParams(t_air=5, wind_speed=3, rel_humidity=0.80, pressure=1000, cloudiness=0.2, incoming_shortwave=250, z=1.6, elev=290, x=478342, y=8655635)
+    print(test_aws_params)
+    print(test_aws_params.Tz)
+    print(test_aws_params.__dataclass_fields__['Tz'].metadata)
+    # or:
+    print(test_aws_params.get_units("Tz"))
+    print(getattr(test_aws_params, "Tz"))
