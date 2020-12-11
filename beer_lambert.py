75a340a0fab0d34a91e2111bedf28abd8c688ff1
tepextepex <tepextepex@gmail.com>
2020-12-11 20:08:18 +0300

    computes absorbing of radiation in the layer between given depths

--- beer_lambert.py
+++ beer_lambert.py
@@ -12,6 +12,19 @@ import math
 import matplotlib.pyplot as plt
 
 
+def absorbed_between(top_depth, bottom_depth, flux_in, density=900):
+    ir_in_solar_flux = 0.36  # infrared radiation is consumed by surface and does not penetrate under it
+    absorbed = False
+    top_out_flux = beer_lambert_for_glacier(flux_in, top_depth, density=density)
+    # print(top_out_flux)
+    bottom_out_flux = beer_lambert_for_glacier(flux_in, bottom_depth, density=density)
+    # print(bottom_out_flux)
+    absorbed = top_out_flux - bottom_out_flux if top_out_flux > bottom_out_flux else bottom_out_flux - top_out_flux
+    if top_depth == 0 or bottom_depth == 0:
+        absorbed += ir_in_solar_flux * flux_in
+    return absorbed
+
+
 def beer_lambert_for_glacier(flux_in, thickness, density=900):
     """
 
@@ -23,8 +36,13 @@ def beer_lambert_for_glacier(flux_in, thickness, density=900):
     ir_in_solar_flux = 0.36  # infrared radiation is consumed by surface and does not penetrate under it
     flux_out = False
     try:
-        k = __extinction_coef(density)
-        flux_out = (1 - ir_in_solar_flux) * __beer_lambert(flux_in, k, thickness)
+        if thickness > 0:
+            k = __extinction_coef(density)
+            flux_out = (1 - ir_in_solar_flux) * __beer_lambert(flux_in, k, thickness)
+        elif thickness == 0:
+            flux_out = (1 - ir_in_solar_flux) * flux_in
+        else:
+            raise ValueError
     except Exception as e:
         pass
     return flux_out
@@ -87,6 +105,7 @@ if __name__ == "__main__":
     # print(__extinction_coef(850))
     # print(beer_lambert_for_glacier(100, 0.22, 900))
     in_flux = 100
+    """
     depths = [-0.05 * x for x in range(0, 45)]
     density = 880
     fluxes = [beer_lambert_for_glacier(in_flux, -x, density=density) for x in depths]
@@ -106,3 +125,9 @@ if __name__ == "__main__":
     plt.ylabel("Depth below glacier surface, m")
     plt.tight_layout()
     plt.show()
+    """
+    a = [x * 0.1 for x in range(0, 10)]
+    for x in range(0, 10):
+        print("Depth: %.2f-%.2f" % (x * 0.2, x * 0.2 + 0.2))
+        absorbed = absorbed_between(x * 0.2, x * 0.2 + 0.2, 100)
+        print("Percent of flux absorbed inside the layer: %.1f" % absorbed)
